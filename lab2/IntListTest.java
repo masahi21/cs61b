d83a211d97bf7a44779915784f720ad22bb3a622
MATTHEW URIAH-TORRES SAHIM <masahi@berkeley.edu>
2020-01-31 10:37:23 -0800

    submitting lab2

--- lab2/IntListTest.java
+++ lab2/IntListTest.java
@@ -41,11 +41,11 @@ public class IntListTest {
     
     @Test
     public void testSquareListRecursive() {
-        // TODO: test for correctness
-        // TODO: test for non-destructiveness
-        
-        // TODO: remove placeholder
-        assertTrue(false);
+        IntList L = IntList.list(1, 2, 3);
+        IntList.squareListRecursive(L);
+        assertEquals(IntList.list(1, 2, 3), L);
+        assertEquals(IntList.list(1, 4, 9), IntList.squareListRecursive(L));
+
     }
 
     /* Run the unit tests in this file. */
