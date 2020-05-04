9f616cc15746852e9400a2f427aad99865e96d33
Matthew Sahim <masahi@berkeley.edu>
2020-05-04 03:12:02 -0700

    test2

--- proj3/testing/tester.py
+++ proj3/testing/tester.py
@@ -84,7 +84,7 @@ TEST.dir).
 When finished, reports number of tests passed and failed, and the number of
 faulty TEST.in files."""
 
-GITLET_COMMAND = "java -ea gitlet.Main.java"
+GITLET_COMMAND = "java -ea gitlet.Main"
 TIMEOUT = 10
 
 def Usage():
